ba42fca3726a63f827c9ade24829eca3bc0671fb
Jan Christoph Ebersbach <jceb@e-jc.de>
2016-04-21 08:21:17 +0200

    Update libraries and add references to websites

--- setup.sh
+++ setup.sh
@@ -24,11 +24,13 @@ chmod a+x fonts/compile.sh
 mkdir -p ISO690_bibliography
 curl -o ISO690_bibliography/doc.html "http://markmail.org/download.xqy?id=n2qumyhk2togibuf&number=1"
 
+# http://ant-contrib.sourceforge.net/
 [ -e ant-contrib ] && rm -rf ant-contrib
 curl -o ant-contrib-1.0b3-src.tar.gz 'http://downloads.sourceforge.net/project/ant-contrib/ant-contrib/1.0b3/ant-contrib-1.0b3-bin.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fant-contrib%2Ffiles%2Fant-contrib%2F1.0b3%2F&ts=1410288260&use_mirror=kent'
 tar xzf ant-contrib-1.0b3-src.tar.gz
 rm ant-contrib-1.0b3-src.tar.gz
 
+# https://xmlgraphics.apache.org/fop/
 [ -e fop-1.1 ] && rm -rf fop-1.1
 [ -e fop-2.0 ] && rm -rf fop-2.0
 [ -e fop-2.1 ] && rm -rf fop-2.1
@@ -38,20 +40,23 @@ tar xzf fop-2.1-bin.tar.gz
 rm fop-2.1-bin.tar.gz
 ln -sf fop-2.1 fop
 
+# http://offo.sourceforge.net/
 [ -e offo-hyphenation-binary ] && rm -rf offo-hyphenation-binary
-curl -o offo-hyphenation-binary.zip 'http://downloads.sourceforge.net/project/offo/offo-hyphenation/2.1/offo-hyphenation-binary.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Foffo%2F&ts=1409909292&use_mirror=softlayer-ams'
+wget -O offo-hyphenation-binary.zip 'http://downloads.sourceforge.net/project/offo/offo-hyphenation/2.2/offo-hyphenation-compiled.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Foffo%2F&ts=1461219320&use_mirror=netix'
 unzip offo-hyphenation-binary.zip
 rm offo-hyphenation-binary.zip
 
+# http://saxon.sourceforge.net/
 [ -e saxon9 ] && rm -rf saxon9
-curl -o SaxonHE9-5-1-7J.zip 'http://downloads.sourceforge.net/project/saxon/Saxon-HE/9.5/SaxonHE9-5-1-7J.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsaxon%2Ffiles%2FSaxon-HE%2F9.5%2F&ts=1409909692&use_mirror=netcologne'
+curl -O 'http://netcologne.dl.sourceforge.net/project/saxon/Saxon-HE/9.7/SaxonHE9-7-0-4J.zip'
 mkdir saxon9
 cd saxon9
-unzip ../SaxonHE9-5-1-7J.zip
+unzip ../SaxonHE9-7-0-4J.zip
 ln -sf saxon9he.jar saxon9.jar
 cd ..
-rm SaxonHE9-5-1-7J.zip
+rm SaxonHE9-7-0-4J.zip
 
+# https://code.google.com/archive/p/jing-trang/
 [ -e jing-20091111 ] && rm -rf jing-20091111
 [ -h jing ] && rm jing
 curl -O 'https://jing-trang.googlecode.com/files/jing-20091111.zip'
@@ -59,16 +64,17 @@ unzip jing-20091111.zip
 rm jing-20091111.zip
 ln -sf jing-20091111 jing
 
+# http://docbook.sourceforge.net/
 [ -e docbook-xsl-1.78.1 ] && rm -rf docbook-xsl-1.78.1
 [ -e docbook-xsl-1.79.0 ] && rm -rf docbook-xsl-1.79.0
 [ -e docbook-xsl-1.79.1 ] && rm -rf docbook-xsl-1.79.1
-[ -e docbook-xsl-ns-1.79.1 ] && rm -rf docbook-xsl-ns-1.79.1
 [ -h docbook-xsl ] && rm docbook-xsl
-curl -O 'http://netcologne.dl.sourceforge.net/project/docbook/docbook-xsl-ns/1.79.1/docbook-xsl-ns-1.79.1.zip'
-unzip docbook-xsl-ns-1.79.1.zip
-rm docbook-xsl-ns-1.79.1.zip
-ln -sf docbook-xsl-ns-1.79.1 docbook-xsl
+curl -O 'http://netcologne.dl.sourceforge.net/project/docbook/docbook-xsl/1.79.1/docbook-xsl-1.79.1.zip'
+unzip docbook-xsl-1.79.1.zip
+rm docbook-xsl-1.79.1.zip
+ln -sf docbook-xsl-1.79.1 docbook-xsl
 
+# http://jeuclid.sourceforge.net/
 [ -e jeuclid-fop-3.1.9 ] && rm -rf jeuclid-fop-3.1.9
 [ -h jeuclid-fop ] && rm jeuclid-fop
 curl -o jeuclid-fop-3.1.9.zip 'http://downloads.sourceforge.net/project/jeuclid/3.1.x/3.1.9/jeuclid-fop-3.1.9-distribution.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fjeuclid%2Ffiles%2F3.1.x%2F3.1.9%2F&ts=1422986063&use_mirror=softlayer-ams'
